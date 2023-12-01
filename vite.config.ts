b9d3b66898a4f81e17f6fafdcbca5d5d1379a53f
p3lo <mysiacik@mysiacik.com>
2023-12-01 23:43:02 +0100

    Fix formatting in postcss.config.mjs, app/lib/utils.ts, and vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import type { ViteDevServer } from "vite";
 
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
 import morgan from "morgan";
+import { remixDevTools } from "remix-development-tools/vite";
 import { defineConfig } from "vite";
 // @ts-expect-error - No types available
 import eslint from "vite-plugin-eslint";
@@ -10,6 +11,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
 	plugins: [
+		remixDevTools(),
 		TurboConsole(),
 		eslint({ fix: true }),
 		morganPlugin(),
