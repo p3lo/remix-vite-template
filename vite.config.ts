833bacb88921875acfdd8194fcc87621445f6d1e
p3lo <mysiacik@mysiacik.com>
2024-01-08 23:31:58 +0100

    Update dependencies and configuration files

--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,32 @@
-import type { ViteDevServer } from "vite";
-
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
-import morgan from "morgan";
-import { remixDevTools } from "remix-development-tools/vite";
-import { defineConfig } from "vite";
-// @ts-expect-error - No types available
-import eslint from "vite-plugin-eslint";
-import TurboConsole from "vite-plugin-turbo-console";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { unstable_vitePlugin as remix } from '@remix-run/dev'
+import morgan from 'morgan'
+import { remixDevTools } from 'remix-development-tools/vite'
+import { defineConfig, type ViteDevServer } from 'vite'
+import TurboConsole from 'vite-plugin-turbo-console'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import remixConfig from './remix.config'
 
 export default defineConfig({
-	plugins: [
-		remixDevTools(),
-		TurboConsole(),
-		eslint({ fix: true }),
-		morganPlugin(),
-		remix(),
-		tsconfigPaths(),
-	],
-	server: {
-		// Automatically opens up the browser when we run npm run dev
-		open: true,
-	},
-});
+  plugins: [
+    remixDevTools(),
+    TurboConsole(),
+    morganPlugin(),
+    remix(remixConfig),
+    tsconfigPaths(),
+  ],
+  server: {
+    // Automatically opens up the browser when we run npm run dev
+    open: true,
+  },
+})
 
 function morganPlugin() {
-	return {
-		name: "morgan-plugin",
-		configureServer(server: ViteDevServer) {
-			return () => {
-				server.middlewares.use(morgan("tiny"));
-			};
-		},
-	};
+  return {
+    name: 'morgan-plugin',
+    configureServer(server: ViteDevServer) {
+      return () => {
+        server.middlewares.use(morgan('tiny'))
+      }
+    },
+  }
 }
