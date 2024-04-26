8c80f7d33809ada92349ce920db670aa72039926
p3lo <mysiacik@mysiacik.com>
2024-04-26 22:05:59 +0200

    Remove unused code and update remix configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { vitePlugin as remix } from '@remix-run/dev'
 import morgan from 'morgan'
 import { remixDevTools } from 'remix-development-tools'
+import { flatRoutes } from 'remix-flat-routes'
 import { defineConfig, type ViteDevServer } from 'vite'
 import TurboConsole from 'vite-plugin-turbo-console'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import remixConfig from './remix.config'
 
 export default defineConfig({
   plugins: [
@@ -12,7 +12,15 @@ export default defineConfig({
     TurboConsole(),
     morganPlugin(),
     tsconfigPaths(),
-    remix(remixConfig),
+    remix({
+      ignoredRouteFiles: ['**/*'],
+      serverModuleFormat: 'esm',
+      routes: async defineRoutes => {
+        return flatRoutes('routes', defineRoutes, {
+          ignoredRouteFiles: ['**/*.test.{js,jsx,ts,tsx}', '**/__*.*'],
+        })
+      },
+    }),
   ],
   server: {
     // Automatically opens up the browser when we run npm run dev
